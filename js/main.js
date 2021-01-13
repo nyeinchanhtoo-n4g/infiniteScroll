c087f990a2ce6e5b432015f76146782d4cb81328
nyeinchanhtoo-n4g <nyeinchanhtoo@hotmail.com>
2021-01-13 13:51:19 +0630

    edit style and search

--- js/main.js
+++ js/main.js
@@ -1,70 +1,65 @@
-let postContainerEL = document.getElementById('post-container');
-let postLoaderEL = document.querySelector('.loader');
-let searchEL = document.getElementById('searchIn');
-
-searchEL.value = '';
+let postSectionEl = document.getElementById(`post-block`);
+let loadingSectionEL = document.querySelector(`.loadingSection`);
+let searchInEl = document.getElementById(`searchIn`);
 
 let postCount = 5;
 let pageCount = 1;
-async function getPost() {
-    let data = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${postCount}&_page=${pageCount}`);
-    return data.json();
+searchInEl.value=``;
+
+async function getPostsData() {
+    let postsData = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${postCount}&_page=${pageCount}`);
+    return postsData.json();
 }
 
-async function showPost() {
-    let posts = await getPost();
-    posts.forEach(post => {
-        let domElement = document.createElement('div');
-        domElement.classList.add('post');
-        domElement.innerHTML = `
-            <div class="post-number">${post.id}</div>
-            <div class="post-info">
-                <div class="post-title">${post.title}</div>
-                <div class="post-body">${post.body}</div>
-            </div>
+async function showPostData() {
+    let posts = await getPostsData();
+    posts.forEach((post) => {
+        let postElupdate = document.createElement(`div`);
+        postElupdate.classList.add(`postSection`);
+        postElupdate.innerHTML = `
+                <div class="postNumber">${post.id}</div>
+                <div class="postInfo">
+                    <div class="postTitle">${post.title}</div>
+                    <div class="postBody">${post.body}</div>
+                </div>
         `;
-        postContainerEL.appendChild(domElement);
+        postSectionEl.appendChild(postElupdate);
     });
 }
 
-showPost();
+showPostData();
 
-window.addEventListener('scroll', (v) => {
-    let {
-        scrollTop,
-        clientHeight,
-        scrollHeight
-    } = document.documentElement;
-    let scrollTotal = scrollTop + clientHeight;
-
-    if (scrollTotal >= scrollHeight) {
+window.addEventListener(`scroll`, (element)=>{
+    let {scrollTop, scrollHeight, clientHeight} = document.documentElement;
+    let scorllSum = scrollTop + clientHeight;
+    if (scorllSum >= scrollHeight) {
         showLoading();
     }
-});
-
-function showLoading() {
-    postLoaderEL.classList.add('show');
-    setTimeout(() => {
-        postLoaderEL.classList.remove('show');
-        setTimeout(() => {
-            pageCount++;
-            showPost();
-        }, 300);
-    }, 1000)
-}
-
-searchEL.addEventListener('input', (e) => {
-    let search = e.target.value.toLowerCase();
-    let posts = document.querySelectorAll('.post');
-
-    posts.forEach(post => {
-        let title = post.querySelector('.post-title').innerText.toLowerCase();
-        let body = post.querySelector('.post-body').innerText.toLowerCase();
-
-        if (title.indexOf(search) > -1 || body.indexOf(search) > -1) {
-            post.style.display = 'flex'
-        } else {
-            post.style.display = 'none'
-        }
-    })
 })
+
+function showLoading(){
+    loadingSectionEL.classList.add(`load`);
+    setTimeout(()=>{
+        loadingSectionEL.classList.remove(`load`);
+
+        setTimeout(()=>{
+            pageCount++;
+            showPostData();
+        },300)
+    },1000);
+};
+
+searchInEl.addEventListener(`input`, (rsp) => {
+    let search = rsp.target.value.toLowerCase();
+    let posts = document.querySelectorAll(`.postSection`);
+    posts.forEach(post => {
+        let title = post.querySelector(`.postTitle`).innerHTML.toLowerCase();
+        let body = post.querySelector(`.postBody`).innerHTML.toLowerCase();
+    
+        if (title.indexOf(search) > -1 || body.indexOf(search) > -1) {
+            post.style.display = `flex`
+        }else {
+            post.style.display = `none`
+        }
+    });
+});
